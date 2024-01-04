7aacf0ba6f7a64056d6a2dd2b0117fd43dd38c15
dhollerbach-nelnet <david.hollerbach@nelnetbank.com>
2024-01-04 10:31:55 -0700

    test

--- scripts/set-env-var.py
+++ scripts/set-env-var.py
@@ -2,8 +2,19 @@ import json
 import subprocess
 import sys
 
-SECRETS = json.loads(sys.argv[1])
+SECRETS_DICT = json.loads(sys.argv[1])
 
-for k, v in SECRETS.items():
-  cmd = 'echo TF_VAR_{}={} >> $GITHUB_ENV'.format(k, v)
-  proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+
+def main(secrets: dict):
+  """Sets secrets as GitHub environment variables. Makes each secret usable by Terraform by prefixing with TF_VAR_.
+
+  Args:
+      secrets (dict): Dictionary of secrets where name = value.
+  """
+  for secret_name, secret_value in secrets.items():
+    cmd = 'echo TF_VAR_{}={} >> $GITHUB_ENV'.format(secret_name, secret_value)
+    subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+
+
+if __name__ == "__main__":
+  main(SECRETS_DICT)
