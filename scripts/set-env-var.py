c85f92060080e0365d4688ae4a14216b56832942
dhollerbach-nelnet <david.hollerbach@nelnetbank.com>
2024-01-04 10:17:41 -0700

    test

--- scripts/set-env-var.py
+++ scripts/set-env-var.py
@@ -2,8 +2,10 @@ import json
 import subprocess
 import sys
 
-SECRETS = json.loads(sys.argv[1])
+SECRETS = sys.argv[1]
 
-for k, v in SECRETS.items():
-  cmd = 'echo TF_VAR_{}={} >> $GITHUB_ENV'.format(k, v)
-  proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+print(SECRETS)
+
+# for k, v in SECRETS.items():
+#   cmd = 'echo TF_VAR_{}={} >> $GITHUB_ENV'.format(k, v)
+#   proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
