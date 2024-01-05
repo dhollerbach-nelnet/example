44baa19c52bef5fda6259bfe903129126e7f2eb1
dhollerbach-nelnet <david.hollerbach@nelnetbank.com>
2024-01-04 18:18:33 -0700

    test

--- scripts/set-env-var.py
+++ scripts/set-env-var.py
@@ -9,7 +9,7 @@ def main(secrets: dict):
   """Sets secrets as GitHub environment variables. Makes each secret usable by Terraform by prefixing with TF_VAR_.
 
   Args:
-      secrets (dict): Dictionary of secrets where name = value.
+      secrets (dict): Dictionary of secrets.
   """
   for secret_name, secret_value in secrets.items():
     cmd = 'echo TF_VAR_{}={} >> $GITHUB_ENV'.format(secret_name, secret_value)
