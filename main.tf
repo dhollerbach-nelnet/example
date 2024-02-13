12541221e624c76c8a7ad3772cd39516ed601311
dhollerbach-nelnet <david.hollerbach@nelnetbank.com>
2024-02-13 13:50:58 -0700

    test

--- main.tf
+++ main.tf
@@ -1,7 +1,25 @@
-provider "aws" {
-  region = "us-east-1"
+provider "aws" {}
+
+resource "aws_s3_bucket" "this" {
+  bucket = "oau2i2ndadv923n--213rvnqv-128"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "12yefgg124rnb2q4"
+
+# GITHUB
+provider "github" {}
+
+data "github_repository" "this" {
+  full_name = "dhollerbach-nelnet-org/example"
+}
+
+resource "github_repository_environment" "this" {
+  repository       = data.github_repository.this.name
+  environment      = "example"
+}
+
+resource "github_actions_environment_secret" "test_secret" {
+  repository       = data.github_repository.repo.name
+  environment      = github_repository_environment.this.environment
+  secret_name      = "test_secret_name"
+  plaintext_value  = "%s"
 }
